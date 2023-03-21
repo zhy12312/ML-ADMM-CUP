37b29214fd09a95f643a5baf16f7a876211aa23f
zhy12312 <1370911498@qq.com>
2023-03-21 12:56:00 +0800

    update

--- admm.py
+++ admm.py
@@ -16,22 +16,22 @@ import scipy.io as sio
 
 
 
-def admm_mmes(y, tau, r, sig, var, Phi, Phi_sum, mu = 0.1, rho = 0.1, denoiser=[], iter_num = 20,
+def admm_mmes(y, tau, r, sig, var, Phi, Phi_sum, mu = 0.01, rho = 0.001, denoiser=[], iter_num = 20,
                  shift_step=2, mmes_iter=[], index = None, X_ori=None, save_path = None):
     Phi_tensor = torch.from_numpy(np.transpose(Phi, (2, 0, 1))).cuda().float()
     y_tensor = torch.from_numpy(y).cuda().float() 
     truth = torch.from_numpy(np.transpose(X_ori, (2, 0, 1))).cuda().float() 
     u = At(y, Phi) 
-    T = u  
+    Z = u   # default start point (initialized value)
     x = u
     v = np.zeros_like(u)
-    b = np.zeros_like(T)
+    b = np.zeros_like(Z)
     begin_time = time.time()
     loss_y_min = 1
     [N1,N2,N3] = X_ori.shape
     follow1=0
     for it in range(iter_num):
-        c = T + b
+        c = Z + b
         yb = A(c, Phi)
         x = c + At(np.divide(y-yb, Phi_sum+mu ),Phi)
         input0 = np.random.uniform(0,1,X_ori.shape)*var        
@@ -40,19 +40,19 @@ def admm_mmes(y, tau, r, sig, var, Phi, Phi_sum, mu = 0.1, rho = 0.1, denoiser=[
         if it==0:
             follow=1
         out, loss_y_iter = mmes_denoiser(loss_y_min,follow,follow1, input0, temp_T, truth, tau, r, sig, var, Phi_tensor, y_tensor, mmes_iter[it], mu, rho, shift_step=shift_step)   
-        T = np.transpose(np.squeeze(out), (1, 2, 0))
-        x_rec = T
-        T = shift(T, shift_step)
-        b = b-(x-T)
+        Z = np.transpose(np.squeeze(out), (1, 2, 0))
+        x_rec = Z
+        Z = shift(Z, shift_step)
+        b = b-(x-Z)
         mu = 0.998 * mu
         psnr_x = psnr_block(X_ori, x_rec)
         end_time = time.time()
-        print('{}-ADMM, Iteration {}, loss = {:.5f}, PSNR = {:.2f}dB, time = {}'.format(denoiser ,it+1, loss_y_iter, psnr_x, (end_time-begin_time)))
+        print('PnP-{}, Iteration {}, loss = {:.5f}, PSNR = {:.2f}dB, time = {}'.format(denoiser ,it+1, loss_y_iter, psnr_x, (end_time-begin_time)))
         if loss_y_iter < loss_y_min and it > iter_num/2+3:
             sio.savemat(save_path + 'scene0{}_{}_{:.2f}.mat'.format(index, it+1, psnr_x),{'x_rec': x_rec})
         if loss_y_iter < loss_y_min:
             loss_y_min = loss_y_iter
-            shutil.move('./model.pth','./model1.pth')
+            shutil.move('./model_params.pth','./model_params1.pth')
             follow1=1
         if it == iter_num-1 or it == iter_num-2:
             sio.savemat(save_path + 'scene0{}_{}_{:.2f}.mat'.format(index, it+1, psnr_x),{'x_rec': x_rec})
